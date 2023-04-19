de64179b0f437fbcbb5121c86feec17bce3e7e53
sbilgic19 <sbilgic19@ku.edu.tr>
2023-04-19 14:58:15 +0300

    hw8 is done

--- project1_PCAT/app.js
+++ project1_PCAT/app.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  const photo = {
-    id: 1,
-    name: 'Photo Name',
-    description: 'Photo Description',
-  };
-  res.send(photo);
-});
+// app.get('/', (req, res) => {
+//   const photo = {
+//     id: 1,
+//     name: 'Photo Name',
+//     description: 'Photo Description',
+//   };
+//   res.send(photo);
+// });
 
 const port = 3000;
 app.listen(port, () => {
--- project2_CleanBlog/app.js
+++ project2_CleanBlog/app.js
@@ -1,11 +1,45 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
+app.use(express.static('public'));
+
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
+  // const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+  // res.send(blog);
+
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
 });
+
+app.get('/about', (req, res) => {
+  // const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+  // res.send(blog);
+
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  // const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+  // res.send(blog);
+
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+  // const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+  // res.send(blog);
+
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('post');
+});
+
 const port = 3001;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı`);
